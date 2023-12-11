404609cd13c3248479f117c83a5aedb342ea3698
meddizzle316 <meddizzle316@gmail.com>
2023-12-11 15:33:01 -0600

    adding my free_stack function

---
+++ free_stack.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: head of list
+ * Return: always void
+ */
+void free_stack(stack_t **head)
+{
+	stack_t *tmp;
+	stack_t *current;
+
+	if (*head)
+	{
+		current = *head;
+		while (current != NULL)
+		{
+			tmp = current->next;
+			free(current);
+			current = tmp;
+		}
+	}
+}
